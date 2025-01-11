6013d45b5a435f87ac100b3ab52fd7176b281bef
Chandrakiran G <121334151+Kiran-VLSI@users.noreply.github.com>
2025-01-11 21:19:18 +0530

    Create FA_RTL.v

---
+++ FullAdder/FA_RTL.v
@@ -0,0 +1,12 @@
+`timescale 1ns / 1ps
+
+module fulladder(
+    input a,
+    input b,
+    input c,
+    output sum,
+    output carry
+    );
+    assign sum = (a^b^c);
+    assign carry = (a&b) | cin&(a^b);
+endmodule
